e58dc1c6051bf6c332692c97f21b9f1f4525d47a
Carsten Block <mail@carstenblock.org>
2011-01-12 18:19:28 +0100

    processPositionUpdate & processCashUpdate implemented and corresponding tests added

--- grails-app/services/org/powertac/accountingservice/AccountingService.groovy
+++ grails-app/services/org/powertac/accountingservice/AccountingService.groovy
@@ -1,6 +1,22 @@
+/*
+ * Copyright 2009-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an
+ * "AS IS" BASIS,  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
 package org.powertac.accountingservice
 
-import org.powertac.common.enumerations.TariffState
+import org.powertac.common.exceptions.CashUpdateException
 import org.powertac.common.exceptions.PositionUpdateException
 import org.powertac.common.*
 import org.powertac.common.command.*
@@ -8,50 +24,62 @@ import org.powertac.common.command.*
 class AccountingService implements org.powertac.common.interfaces.AccountingService {
 
   PositionUpdate processPositionUpdate(PositionDoUpdateCmd positionDoUpdateCmd) throws PositionUpdateException {
-    assert (positionDoUpdateCmd.validate())
-    BigDecimal overallBalance = 0.0
+    if (!positionDoUpdateCmd) return null
+    if (!positionDoUpdateCmd.validate()) throw new PositionUpdateException("Failed to validate incoming PositionDoUpdateCommand: ${positionDoUpdateCmd.errors.allErrors}")
+    try {
+      BigDecimal overallBalance = 0.0
+      PositionUpdate oldPositionUpdate = PositionUpdate.withCriteria(uniqueResult: true) {
+        eq('competition', positionDoUpdateCmd.competition)
+        eq('broker', positionDoUpdateCmd.broker)
+        eq('product', positionDoUpdateCmd.product)
+        eq('timeslot', positionDoUpdateCmd.timeslot)
+        eq('latest', true)
+      }
+      if (oldPositionUpdate) {
+        oldPositionUpdate.latest = false
+        oldPositionUpdate.save()
+        overallBalance = oldPositionUpdate.overallBalance
+      }
 
-    PositionUpdate oldPositionUpdate = PositionUpdate.withCriteria(uniqueResult: true) {
-      eq('competition', positionDoUpdateCmd.competition)
-      eq('broker', positionDoUpdateCmd.broker)
-      eq('product', positionDoUpdateCmd.product)
-      eq('timeslot', positionDoUpdateCmd.timeslot)
-      eq('latest', true)
-    }
-    if (oldPositionUpdate) {
-      oldPositionUpdate.latest = false
-      oldPositionUpdate.save()
-      overallBalance = oldPositionUpdate.overallBalance
-    }
+      def positionUpdate = new PositionUpdate(positionDoUpdateCmd.properties)
+      positionUpdate.overallBalance = (overallBalance + positionDoUpdateCmd.relativeChange)
+      positionUpdate.transactionId = IdGenerator.createId()
+      positionUpdate.latest = true
+      assert (positionUpdate.validate() && positionUpdate.save())
+      return positionUpdate
 
-    def positionUpdate = new PositionUpdate(positionDoUpdateCmd.properties)
-    positionUpdate.overallBalance = (overallBalance + positionDoUpdateCmd.relativeChange)
-    positionUpdate.transactionId = IdGenerator.createId()
-    positionUpdate.latest = true
-    assert (positionUpdate.validate() && positionUpdate.save())
-    return null
+    } catch (Exception ex) {
+      throw new PositionUpdateException('An error occurred during processPositionUpdate.', ex)
+    }
   }
 
-  CashUpdate processCashUpdate(CashDoUpdateCmd cashDoUpdateCmd) {
-    assert (cashDoUpdateCmd.validate())
-    BigDecimal overallBalance = 0.0
-    CashUpdate oldCashUpdate = CashUpdate.withCriteria(uniqueResult: true) {
-      eq('competition', cashDoUpdateCmd.competition)
-      eq('broker', cashDoUpdateCmd.broker)
-      eq('latest', true)
-    }
-    if (oldCashUpdate) {
-      oldCashUpdate.latest = false
-      oldCashUpdate.save()
-      overallBalance = oldCashUpdate.overallBalance
+  CashUpdate processCashUpdate(CashDoUpdateCmd cashDoUpdateCmd) throws CashUpdateException {
+    if (!cashDoUpdateCmd) return null
+    if (!cashDoUpdateCmd.validate()) throw new CashUpdateException("Failed to validate incoming CashDoUpdateCmd: ${cashDoUpdateCmd.errors.allErrors}")
+    try {
+      BigDecimal overallBalance = 0.0
+      CashUpdate oldCashUpdate = CashUpdate.withCriteria(uniqueResult: true) {
+        eq('competition', cashDoUpdateCmd.competition)
+        eq('broker', cashDoUpdateCmd.broker)
+        eq('latest', true)
+      }
+      if (oldCashUpdate) {
+        oldCashUpdate.latest = false
+        oldCashUpdate.save()
+        overallBalance = oldCashUpdate.overallBalance
+      }
+
+      def cashUpdate = new CashUpdate(cashDoUpdateCmd.properties)
+      cashUpdate.overallBalance = (overallBalance + cashDoUpdateCmd.relativeChange)
+      cashUpdate.transactionId = IdGenerator.createId()
+      cashUpdate.latest = true
+      assert (cashUpdate.validate() && cashUpdate.save())
+      return cashUpdate
+
+    } catch (Exception ex) {
+      throw new CashUpdateException('An error occurred during processCashUpdate.', ex)
     }
 
-    def cashUpdate = new CashUpdate(cashDoUpdateCmd.properties)
-    cashUpdate.overallBalance = overallBalance
-    cashUpdate.transactionId = IdGenerator.createId()
-    cashUpdate.latest = true
-    assert (cashUpdate.validate() && cashUpdate.save())
-    return cashUpdate
   }
 
   void processTariffPublished(TariffDoPublishCmd tariffDoPublishCmd) {
@@ -89,14 +117,4 @@ class AccountingService implements org.powertac.common.interfaces.AccountingServ
       return Customer.findAllByCompetition(competition)
     }
   }
-
-  void competitionBeforeStart(Competition competition) { }
-
-  void competitionAfterStart(Competition competition) { }
-
-  void competitionBeforeStop(Competition competition) { }
-
-  void competitionAfterStop(Competition competition) { }
-
-  void competitionReset(Competition competition) { }
 }
--- test/integration/org/powertac/accountingservice/AccountingServiceTests.groovy
+++ test/integration/org/powertac/accountingservice/AccountingServiceTests.groovy
@@ -1,17 +1,150 @@
+/*
+ * Copyright 2009-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an
+ * "AS IS" BASIS,  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+
+
 package org.powertac.accountingservice
 
-import grails.test.*
+import org.powertac.common.command.CashDoUpdateCmd
+import org.powertac.common.command.PositionDoUpdateCmd
+import org.powertac.common.enumerations.ProductType
+import org.powertac.common.exceptions.CashUpdateException
+import org.powertac.common.exceptions.PositionUpdateException
+import org.powertac.common.*
 
 class AccountingServiceTests extends GroovyTestCase {
-    protected void setUp() {
-        super.setUp()
-    }
 
-    protected void tearDown() {
-        super.tearDown()
-    }
+  AccountingService accountingService
 
-    void testSomething() {
+  Competition competition
+  Product product
+  Timeslot timeslot
+  Broker broker
+  String userName
+  String apiKey
 
+
+  protected void setUp() {
+    super.setUp()
+    userName = 'testBroker'
+    apiKey = 'testApiKey-which-needs-to-be-longer-than-32-characters'
+    competition = new Competition(name: "test")
+    assert (competition.validate() && competition.save())
+    broker = new Broker(competition: competition, userName: userName, apiKey: apiKey)
+    assert (broker.validate() && broker.save())
+    product = new Product(competition: competition, productType: ProductType.Future)
+    assert (product.validate() && product.save())
+    timeslot = new Timeslot(competition: competition, serialNumber: 0)
+    assert (timeslot.validate() && timeslot.save())
+  }
+
+  protected void tearDown() {
+    super.tearDown()
+  }
+
+  void testAccountingServiceNotNull() {
+    assertNotNull(accountingService)
+  }
+
+
+  //processCashUpdate tests
+  void testProcessCashUpdateNull() {
+    assertNull(accountingService.processCashUpdate(null))
+  }
+
+  void testProcessCashUpdateInvalidCommandObject() {
+    shouldFail(CashUpdateException) {
+      accountingService.processCashUpdate(new CashDoUpdateCmd())
     }
+  }
+
+  void testProcessCashUpdateValidCommandNoPreviousPosition() {
+    CashDoUpdateCmd cmd = new CashDoUpdateCmd(competition: competition, broker: broker, relativeChange: 1.0, reason: 'someReason', origin: 'someOrigin')
+    assertTrue (cmd.validate())
+    def cashUpdate = accountingService.processCashUpdate(cmd)
+    assertEquals(1.0, cashUpdate.relativeChange)
+    assertEquals(1.0, cashUpdate.overallBalance)
+    assertTrue(cashUpdate.latest)
+    assertNotNull(cashUpdate.transactionId)
+    assertEquals('someReason', cashUpdate.reason)
+    assertEquals('someOrigin', cashUpdate.origin)
+    assertEquals(1, CashUpdate.count())
+  }
+
+  void testProcessCashUpdateValidCommandWithPreviousPositions() {
+    CashUpdate cashUpdate1 = new CashUpdate(competition: competition, broker: broker, relativeChange: 1.0, overallBalance: 1.0, latest: true, transactionId: 'someTransactionId')
+    assertTrue (cashUpdate1.validate() && cashUpdate1.save())
+
+    CashDoUpdateCmd cmd = new CashDoUpdateCmd(competition: competition, broker: broker, relativeChange: -2.0, reason: 'someReason', origin: 'someOrigin')
+    assertTrue (cmd.validate())
+
+    def cashUpdate2 = accountingService.processCashUpdate(cmd)
+    assertEquals(-2.0, cashUpdate2.relativeChange)
+    assertEquals(-1.0, cashUpdate2.overallBalance)
+    assertTrue(cashUpdate2.latest)
+    assertFalse(cashUpdate1.latest)
+    assertNotNull(cashUpdate2.transactionId)
+    assertEquals('someReason', cashUpdate2.reason)
+    assertEquals('someOrigin', cashUpdate2.origin)
+    assertEquals(2, CashUpdate.count())
+  }
+
+
+
+  // processPositionUpdate tests
+  void testProcessPositionUpdateNull() {
+    assertNull(accountingService.processPositionUpdate(null))
+  }
+
+  void testProcessPositionUpdateInvalidCommandObject() {
+    shouldFail(PositionUpdateException) {
+      accountingService.processPositionUpdate(new PositionDoUpdateCmd())
+    }
+  }
+
+  void testProcessPositionUpdateValidCommandNoPreviousPosition() {
+    PositionDoUpdateCmd cmd = new PositionDoUpdateCmd(competition: competition, broker: broker, product: product, timeslot: timeslot, relativeChange: 1.0, reason: 'someReason', origin: 'someOrigin')
+    assertTrue (cmd.validate())
+    def positionUpdate = accountingService.processPositionUpdate(cmd)
+    assertEquals(1.0, positionUpdate.relativeChange)
+    assertEquals(1.0, positionUpdate.overallBalance)
+    assertTrue(positionUpdate.latest)
+    assertNotNull(positionUpdate.transactionId)
+    assertEquals('someReason', positionUpdate.reason)
+    assertEquals('someOrigin', positionUpdate.origin)
+    assertEquals(1, PositionUpdate.count())
+  }
+
+  void testProcessPositionUpdateValidCommandWithPreviousPositions() {
+    PositionUpdate positionUpdate1 = new PositionUpdate(competition: competition, broker: broker, product: product, timeslot: timeslot, relativeChange: 1.0, overallBalance: 1.0, latest: true, transactionId: 'someTransactionId')
+    assertTrue (positionUpdate1.validate() && positionUpdate1.save())
+
+    PositionDoUpdateCmd cmd = new PositionDoUpdateCmd(competition: competition, broker: broker, product: product, timeslot: timeslot, relativeChange: -2.0, reason: 'someReason', origin: 'someOrigin')
+    assertTrue (cmd.validate())
+
+    def positionUpdate2 = accountingService.processPositionUpdate(cmd)
+    assertEquals(-2.0, positionUpdate2.relativeChange)
+    assertEquals(-1.0, positionUpdate2.overallBalance)
+    assertTrue(positionUpdate2.latest)
+    assertFalse(positionUpdate1.latest)
+    assertNotNull(positionUpdate2.transactionId)
+    assertEquals('someReason', positionUpdate2.reason)
+    assertEquals('someOrigin', positionUpdate2.origin)
+    assertEquals(2, PositionUpdate.count())
+  }
+
+
 }
