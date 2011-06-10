628827f86fe9bbd67ad46cfaf4a2bf3695276f1a
John Collins <jcollins@cs.umn.edu>
2011-06-10 12:02:43 -0500

    switch to remote dependencies

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -2,9 +2,9 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
-grails.plugin.location.PowertacCommon = "../powertac-common"
-grails.plugin.location.PowertacRandom = "../powertac-random"
-grails.plugin.location.PowertacServerInterface = "../powertac-server-interface"
+//grails.plugin.location.PowertacCommon = "../powertac-common"
+//grails.plugin.location.PowertacRandom = "../powertac-random"
+//grails.plugin.location.PowertacServerInterface = "../powertac-server-interface"
 
 grails.project.dependency.resolution = {
   // inherit Grails' default dependencies
@@ -38,5 +38,8 @@ grails.project.dependency.resolution = {
   
   plugins {
     build (':joda-time:1.1')
+    build (':powertac-common:0.10')
+    build (':powertac-random:0.2')
+    build (':powertac-server-interface:0.2')
   }
 }
